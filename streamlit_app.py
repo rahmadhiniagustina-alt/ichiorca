3aaac4140bde881d0878202aab24799481a325a4
rahmadhiniagustina-alt <rahmadhiniagustina@gmail.com>
2026-05-22 14:16:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,8 @@ st.header("Three", divider=True)
 st.header("Four", divider=True)
 
 st.balloons()
+
+st.page_link("your_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
